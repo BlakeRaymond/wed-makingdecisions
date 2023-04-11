7af02c7c49a3af6ad8ab23e95d9fd7f90337952e
Blake Raymond <blake.crypto@technologist.com>
2023-04-11 16:37:23 -0600

    smooth operators

---
+++ makdec1.js
@@ -0,0 +1,10 @@
+let x = 5
+let y = 5
+
+if(x>y){
+    console.log(`${x} is greater than ${y}`);
+}else if(x<y){
+    console.log(`${y} is greater than ${x}`);
+}else {
+    console.log("The numbers are equal.")
+}
